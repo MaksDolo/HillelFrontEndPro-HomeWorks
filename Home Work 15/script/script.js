4d1f005dfb2fe67a2bd1491cd23c4f220390aaa2
Maksym Dolomanchuk <maksdoloman0101@gmail.com>
2024-01-15 23:07:16 +0200

    correction

--- Home Work 15/script/script.js
+++ Home Work 15/script/script.js
@@ -1,71 +1,73 @@
-const users = [
+let users = [
     {
-        index: 0,
-        isActive: true,
-        balance: 2227,
-        name: "Eugenia Sawyer",
-        gender: "female",
-        phone: "+1 (840) 583-3207",
-        address: "949 John Street, Rose, Puerto Rico, 1857"
+        "index": 0,
+        "isActive": true,
+        "balance": "$2,226.60",
+        "name": "Eugenia Sawyer",
+        "gender": "female",
+        "phone": "+1 (840) 583-3207",
+        "address": "949 John Street, Rose, Puerto Rico, 1857"
     },
     {
-        index: 1,
-        isActive: true,
-        balance: 2614,
-        name: "Pauline Gallegos",
-        gender: "female",
-        phone: "+1 (985) 593-3328",
-        address: "328 Greenpoint Avenue, Torboy, North Dakota, 6857"
+        "index": 1,
+        "isActive": true,
+        "balance": "$2,613.77",
+        "name": "Pauline Gallegos",
+        "gender": "female",
+        "phone": "+1 (985) 593-3328",
+        "address": "328 Greenpoint Avenue, Torboy, North Dakota, 6857"
     },
     {
-        index: 2,
-        isActive: false,
-        balance: 3976,
-        name: "Middleton Chaney",
-        gender: "male",
-        phone: "+1 (995) 591-2478",
-        address: "807 Fleet Walk, Brutus, Arkansas, 9783"
+        "index": 2,
+        "isActive": false,
+        "balance": "$3,976.41",
+        "name": "Middleton Chaney",
+        "gender": "male",
+        "phone": "+1 (995) 591-2478",
+        "address": "807 Fleet Walk, Brutus, Arkansas, 9783"
     },
     {
-        index: 3,
-        isActive: true,
-        balance: 1935,
-        name: "Burns Poole",
-        gender: "male",
-        phone: "+1 (885) 559-3422",
-        address: "730 Seba Avenue, Osage, Alabama, 6290"
+        "index": 3,
+        "isActive": true,
+        "balance": "$1,934.58",
+        "name": "Burns Poole",
+        "gender": "male",
+        "phone": "+1 (885) 559-3422",
+        "address": "730 Seba Avenue, Osage, Alabama, 6290"
     },
     {
-        index: 4,
-        isActive: true,
-        balance: 3262,
-        name: "Mcfadden Horne",
-        gender: "male",
-        phone: "+1 (942) 565-3988",
-        address: "120 Scholes Street, Kirk, Michigan, 1018"
+        "index": 4,
+        "isActive": true,
+        "balance": "$3,261.65",
+        "name": "Mcfadden Horne",
+        "gender": "male",
+        "phone": "+1 (942) 565-3988",
+        "address": "120 Scholes Street, Kirk, Michigan, 1018"
     },
     {
-        index: 5,
-        isActive: false,
-        balance: 1791,
-        name: "Suzette Lewis",
-        gender: "female",
-        phone: "+1 (837) 586-3283",
-        address: "314 Dunne Place, Bawcomville, Guam, 9053"
+        "index": 5,
+        "isActive": false,
+        "balance": "$1,790.56",
+        "name": "Suzette Lewis",
+        "gender": "female",
+        "phone": "+1 (837) 586-3283",
+        "address": "314 Dunne Place, Bawcomville, Guam, 9053"
     }
 ];
 
 const filterBalanceUsers = users.filter(function(name) {
-    return name.balance >= 2000;
+    const balanceNumber = parseFloat(name.balance.split('$').join('').split(',').join(''));
+    return balanceNumber >= 2000;
 });
 const usersPhoneNumber = filterBalanceUsers.map(function (name) {
     return name.phone;
 });
-const sumUsersBalance = filterBalanceUsers.reduce(function (sum, name) {
-    return sum + name.balance;
+const sumUsersBalance = users.reduce(function (sum, name) {
+    const balanceNumber = parseFloat(name.balance.split('$').join('').split(',').join(''));
+    return sum + balanceNumber;
 }, 0);
 
 console.log(`Телефоны пользователей у которых баланс больше 2000$:
     ${usersPhoneNumber.join('     ')}`
 );
-console.log(`Сумма баланса этих пользователей: ${sumUsersBalance}`);
+console.log(`Общая сумма балансов пользователей: ${Math.floor(sumUsersBalance)}$`);
